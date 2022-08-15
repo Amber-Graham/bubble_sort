b5805b262e6a98287b16fb8fe076da9cb085ec01
Amber-Graham <ang190045@gmail.com>
2022-08-15 09:11:35 -0500

    remove additional comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,3 @@
-#build a #bubble_sort method that takes an array and returns a sorted array
-#must use the bubble sort methodology
-
-#initialize a flag variable that if 'sorted' is true, it stops iterating
-
 #procedure bubble_sort(array of numbers or items)
 #  n = length of the array
 #  if array index -1 is greater than array index
@@ -10,15 +5,9 @@
 #    swap(array index -1, array index)
 #  end
 #  check to see if everything is in the right order?
+#  if it is, break and return array
 #end
 
-
-#def bubble_sort([array])
-#  n = array.length
-#  if a[i - 1] > a[i]
-#    swap(a[i-1], a[i])
-#  end
-#end
 
 def bubble_sort(array)
   array_length = array.size
