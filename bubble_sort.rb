1869ef29654b8a9c66c1e0aca3612ce3594fcd81
Amber-Graham <ang190045@gmail.com>
2022-08-15 08:41:03 -0500

    initial pseudocode

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+#build a #bubble_sort method that takes an array and returns a sorted array
+#must use the bubble sort methodology
+
+#initialize a flag variable taht if 'sorted' is true, it stops iterating
+
+procedure bubble_sort(array of numbers or items)
+  n = length of the array
+  if array index -1 is greater than array index
+    swap them and remember it changed
+    swap(array index -1, array index)
+  end
+  check to see if everything is in the right order?
+end
+
+
+def bubble_sort([array])
+  n = array.length
+  if a[i - 1] > a[i]
+    swap(a[i-1], a[i])
+  end
+end
